8b430910e8dff3fe2a248e54a331fed9b0239c62
Ant101 <ant-work1@yandex.ru>
2016-12-21 20:29:18 +0300

    Homework 1

---
+++ Selected/DZ1.java
@@ -0,0 +1,38 @@
+/*
+ * Author Malyshev Anton. HW n.1
+ * 06.10.16
+ * Write a metod, which defines if year leap or not. Every 4th year is leap, except every 100th, and every 400th is leap.
+ */
+
+import java.util.Scanner;
+ 
+class DZ1 {
+
+    static int year = 0;
+    static String leapOrNot;
+
+    public static void main (String[] args) {
+        boolean state = true;
+        while (state) {
+            readData();
+            writeData();
+        }
+    }
+    
+    static void readData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        year = sc.nextInt();
+    }
+    
+    static boolean leapCalc (int year) { 
+        return ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 );
+    }
+    
+    static void writeData() {
+        leapOrNot = leapCalc(year) == true ? "leap" : "not leap";
+        System.out.println("Year " + year + " is " + leapOrNot);
+    }
+}
+
+//
